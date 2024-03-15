469a0c268a48d4377b4d0db46ae0abbdd59c1b02
Vyshnavi Meda <vyshnavi.meda@tigeranalytics.com>
2024-03-15 14:53:52 +0530

    add the changes

--- simple_functions.py
+++ simple_functions.py
@@ -1,21 +1,23 @@
 # Custom python functions
 
+
 def double_number(a):
-     '''
+    """
     Doubles the number and returns the answer
     Args:
         a (int) : number
     Returns:
         int : Double the number
-    '''
-    return a+a
+    """
+    return a + a
+
 
 def square_number(a):
-     '''
+    """
     Squares the number and returns the answer
     Args:
         a (int) : number
     Returns:
         int : Squares the number
-    '''
-    return a*a
+    """
+    return a * a
